5235838c6ce6e08c9417ba9e7ff94a0117acd106
Vivian Ding <vyd2@cornell.edu>
2023-10-31 15:14:48 -0400

    Add test

--- s4s/src/main/kotlin/Main.kt
+++ s4s/src/main/kotlin/Main.kt
@@ -1,15 +1,18 @@
 import bottomup.BottomUp
 import util.*
-import java.io.FileOutputStream
-import java.io.PrintWriter
 
 // Note: reflection throws an exception if this is declared inside the function where it's used
-fun f(x: MutableList<Int>, y: Int): List<Int> {
+fun add(x: MutableList<Int>, y: Int): List<Int> {
     x.add(y)
     return x
 }
 
-object impl : UPrimImpl {
+fun addAll(x: MutableList<Int>, y: List<Int>): List<Int> {
+    x.addAll(y)
+    return x
+}
+
+object ListImpl : UPrimImpl {
     override fun len(x: Any): Int =
         when (x) {
             is List<*> -> x.size
@@ -17,42 +20,51 @@ object impl : UPrimImpl {
         }
 }
 
-fun s(x: List<*>): Int = x.size
-
 fun main(args: Array<String>) {
-    val t = Type(listOf(MutableList::class, Int::class), List::class)
-    val e0 = Example(listOf(mutableListOf(1, 2, 3), 5), listOf(1, 2, 3, 5))
-    val e1 = Example(listOf(mutableListOf(1, 2), 3), listOf(1, 2, 3))
-    val e2 = Example(listOf(mutableListOf<Int>(), 3), listOf(3))
-
-    val n0 = Example(listOf(mutableListOf(1, 2), 3), listOf(1, 2, 3, 4))
-
-    println(checkEx(e0, t))
-    println(checkEx(e1, t))
-    println(checkEx(e2, t))
-    println(checkFn(::f, t))
-
-    val query = Query(::f, t, listOf(e0, e1, e2), listOf(n0), impl)
-
-    BottomUp(query).enumerate(6)
-//    val e2 = Example(listOf(listOf(1, 2, 3)), 3)
-//    val t2 = Type(listOf(List::class), Int::class)
-//    println(checkEx(e2, t2))
-//    println(checkFn(::s, t2))
-
-//    val e = Example(listOf(2,3), 4)
-//    val t = Type(listOf(Boolean::class, Int::class), Int::class)
-//    println(checkEx(e, t))
-//    println(checkFn(::f, t))
-//
-//    val e2 = Example(listOf(listOf(1, 2, 3)), 3)
-//    val t2 = Type(listOf(List::class), Int::class)
-//    println(checkEx(e2, t2))
-//    println(checkFn(::s, t2))
+    testAdd()
+    testAddAll()
+//    testDouble()
 
 //    val input = generateSequence(::readLine).joinToString("\n")
 //    val jsonElement = Json.parseToJsonElement(input)
 //    val rawProgram = Json.decodeFromJsonElement<RawProgram>(jsonElement)
 //    val prettyJsonPrinter = Json { prettyPrint = true }
 //    println(prettyJsonPrinter.encodeToString(optimizedProgram))
+}
+
+fun testAdd() {
+    println("Running test for MutableList.add")
+    val t = Type(listOf(MutableList::class, Int::class), List::class)
+
+    val posExamplesAdd = mutableListOf<Example>()
+    posExamplesAdd.add(Example(listOf(mutableListOf(1, 2, 3), 5), listOf(1, 2, 3, 5)))
+    posExamplesAdd.add(Example(listOf(mutableListOf(1, 2), 3), listOf(1, 2, 3)))
+    posExamplesAdd.add(Example(listOf(mutableListOf<Int>(), 3), listOf(3)))
+
+    val negExamplesAdd = mutableListOf<Example>()
+    negExamplesAdd.add(Example(listOf(mutableListOf(1, 2), 3), listOf(1, 2, 3, 4)))
+
+    val queryAdd = Query(::add, t, posExamplesAdd, negExamplesAdd, ListImpl)
+
+    BottomUp(queryAdd).enumerate(6)
+}
+
+fun testAddAll() {
+    println("Running test for MutableList.addAll")
+    val t = Type(listOf(MutableList::class, List::class), List::class)
+
+    val posExamplesAddAll = mutableListOf<Example>()
+    posExamplesAddAll.add(Example(listOf(mutableListOf(1, 2, 3), listOf(5)), listOf(1, 2, 3, 5)))
+    posExamplesAddAll.add(Example(listOf(mutableListOf(1, 2), listOf()), listOf(1, 2)))
+    posExamplesAddAll.add(Example(listOf(mutableListOf(), listOf(3)), listOf(3)))
+    posExamplesAddAll.add(Example(listOf(mutableListOf(1, 2, 3), listOf(5, 7, 9)), listOf(1, 2, 3, 5, 7, 9)))
+
+    val negExamplesAddAll = mutableListOf<Example>()
+    negExamplesAddAll.add(Example(listOf(mutableListOf(1, 2), listOf(3)), listOf(1, 2, 3, 4)))
+    negExamplesAddAll.add(Example(listOf(mutableListOf(1, 2), listOf(3)), listOf(1, 2)))
+    negExamplesAddAll.add(Example(listOf(mutableListOf(1, 2), listOf()), listOf(1, 2, 3)))
+
+    val queryAddAll = Query(::addAll, t, posExamplesAddAll, negExamplesAddAll, ListImpl)
+
+    BottomUp(queryAddAll).enumerate(6)
 }
