04a7c5ae1606afaf3b53168eb1230703300ae89b
Vivian Ding <vyd2@cornell.edu>
2023-11-08 12:07:09 -0500

    Init step2

---
+++ s4s/src/main/kotlin/step2/TopDown.kt
@@ -0,0 +1,11 @@
+package step2
+
+
+/**
+ * Now that we're done with step 1, it's time for a step 2!
+ *
+ * Kinda sad this can't be called bottoms up since it's a top-down synthesizer
+ */
+class TopDown {
+
+}
